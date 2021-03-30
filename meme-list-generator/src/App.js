55b946dd7b183627737b4bbe76c5f7d87e0f87ad
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-29 20:03:33 -0600

    Update App.js

--- meme-list-generator/src/App.js
+++ meme-list-generator/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React from "react";
+import MemeComponent from "./memeComponent";
+import MemeList from "./MemeList";
+
+
+const App = () => {
+
+    return (
+      <div>
+        <h1>Meme Generator</h1>
+        <MemeComponent />
+        <MemeList />
+      </div>
+    )
+  
 }
 
 export default App;
