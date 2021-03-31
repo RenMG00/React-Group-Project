907d30ce9b687d3869c994c8568bae929375bd5e
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-03-30 19:48:28 -0600

    Create MemeComp.js

---
+++ meme-list-generator/src/MemeComp.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import MemeList from "./MemeList";
+import Form from "./Form";
+
+class MemeComp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            currentMeme: [],
+            randomMemes: [],
+            memeList: []
+        }
+    }
+
+
+    componentDidMount() {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.data);
+                let   { memes }  = response.data;
+                this.setState({
+                    randomMemes: memes,
+                    currentMeme: memes[Math.floor(Math.random()* this.state.randomMemes.length)]
+                })
+                console.log(this.state.currentMeme, 111);
+            })
+    }
+
+   
+    createMeme = e => {
+        e.preventDefault()
+
+        const newMeme = {
+            id: this.state.currentMeme.id,
+            topText: this.state.topText,
+            bottomText: this.state.bottomText,
+            url: this.state.currentMeme.url,
+            edited: false
+        }
+
+        this.setState(prevState => ({
+            ...prevState,
+            memeList: [...prevState.memeList, newMeme]
+        }))
+    }
+    handleChange = e => {
+        console.log(e.target)
+        this.setState(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    refresh = e => {
+        e.preventDefault();
+        this.setState({
+            topText: "",
+            bottomText: "",
+            currentMeme: this.state.randomMemes[Math.floor(Math.random()* this.state.randomMemes.length)]
+        })
+    }
+
+  
+    render() {
+        
+        return (
+            <div>
+             
+                <div className="meme">
+                    <img src={this.state.currentMeme.url} alt="" />
+                </div>
+                <div className="form-container">
+                    <Form
+                        topText={this.state.topText}
+                        bottomText={this.state.bottomText}
+                        handleChange={this.handleChange}
+                        createMeme={this.createMeme}
+                    />
+                    <div>
+                        <MemeList props={this.state} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MemeComp;
