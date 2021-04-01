947b1d20d166c582d67a439b442d528ec9759098
Ashley Stanley <astanley006@gmail.com>
2021-04-01 14:05:31 -0400

    memelist update

--- meme-list-generator/src/memeList.js
+++ meme-list-generator/src/memeList.js
@@ -1,13 +1,33 @@
 import React from "react";
-import Meme from "./Meme";
 
-const MemeList = props => {
+function MemeList(props) {
 
-    const memeList = props.memes.map((meme, i) => <Meme key = {i} {...meme} />);
-         
     return (
         <div>
-            {memeList}
+            <div className='memeContainer'>
+                <h2 className="top">{props.topText}</h2>
+                <img src={props.url} alt="" />
+                <h2 className="bottom">{props.bottomText}</h2>
+                <div>
+                    {/* <button onClick={() => props.handleEdit(props.meme.id)}>Edit</button> */}
+                    <button onClick={() => props.handleDelete(props.meme.id)}>Delete</button>
+                </div>
+                <form className="editForm" onSubmit={console.log("hi")}>
+                    <input
+                        name='topText'
+                        type='text'
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        name='bottomText'
+                        type='text'
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">Edit Meme</button>
+                </form>
+            </div>
         </div>
     )
 }
