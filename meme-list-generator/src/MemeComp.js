5cb9d51dbd2f3d734f2fdbd3d4ae118c9b426380
RenMG00 <56203417+RenMG00@users.noreply.github.com>
2021-04-06 10:30:19 -0600

    Update MemeComp.js

--- meme-list-generator/src/MemeComp.js
+++ meme-list-generator/src/MemeComp.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 import MemeList from "./MemeList";
 import Form from "./Form";
 
+
 class MemeComp extends Component {
-    constructor() {
-        super()
-        this.state = {
-            topText: "",
-            bottomText: "",
-            currentMeme: [],
-            randomMemes: [],
-            memeList: []
-        }
+    state = {
+        topText: "",
+        bottomText: "",
+        currentMeme: [],
+        randomMemes: [],
+        memeList: []
     }
 
+<<<<<<< HEAD
     componentDidMount() {
+=======
+// Mounts random meme image to page
+    componentDidMount = () => {
+>>>>>>> 9b2a30295f18efbe1e8057793719c820b6252edd
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
             .then(response => {
@@ -26,28 +29,37 @@ class MemeComp extends Component {
             })
     }
 
+<<<<<<< HEAD
+=======
+// This function should allow the user to enter their data and submit as a caption for the meme
+>>>>>>> 9b2a30295f18efbe1e8057793719c820b6252edd
     createMeme = e => {
         e.preventDefault()
-
+        // New meme variable
         const newMeme = {
             id: this.state.currentMeme.id,
             topText: this.state.topText,
             bottomText: this.state.bottomText,
             url: this.state.currentMeme.url
         }
+<<<<<<< HEAD
+=======
+        // State change upon meme submission
+>>>>>>> 9b2a30295f18efbe1e8057793719c820b6252edd
         this.setState(prevState => ({
             ...prevState,
             memeList: [...prevState.memeList, newMeme]
         }))
         this.refresh(e)
     }
+    // Handle Change function used to display preview
     handleChange = e => {
         const { name, value } = e.target
-        this.setState(({
+        this.setState({
             [name]: value
-        }))
+        })
     }
-
+    // refreshes meme image
     refresh = e => {
         e.preventDefault();
         this.setState({
@@ -80,6 +92,7 @@ class MemeComp extends Component {
 
 
     render() {
+<<<<<<< HEAD
         let memesList = this.state.memeList.map(meme => {
             return <MemeList
                 topText={meme.topText}
@@ -93,6 +106,14 @@ class MemeComp extends Component {
         return (
             <>
                 <div className="form-container">
+=======
+        console.log(this.state)
+        const memeList = this.state.memeList.map((meme) => { return <MemeList meme = {meme.id} img={meme.url}/>})
+        return (
+            <>
+                <img className="meme" src={this.state.currentMeme.url} alt="" value={this.state.name} onChange={this.handleChange}/>
+                <div className="form-container" >
+>>>>>>> 9b2a30295f18efbe1e8057793719c820b6252edd
                     <Form
                         topText={this.state.topText}
                         bottomText={this.state.bottomText}
@@ -105,7 +126,11 @@ class MemeComp extends Component {
                         <h2 className="bottom">{this.state.bottomText}</h2>
                     </div>
                     <div>
+<<<<<<< HEAD
                         {memesList}
+=======
+                        {memeList}
+>>>>>>> 9b2a30295f18efbe1e8057793719c820b6252edd
                     </div>
                 </div>
             </>
